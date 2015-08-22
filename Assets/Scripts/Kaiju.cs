33e91fade4a4c7160aac3c254a9141995d72d06e
Gareth Williams <gareth@garethw.net>
2015-08-22 11:07:26 +0100

    Some basic stuff

---
+++ Assets/Scripts/Kaiju.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kaiju : MonoBehaviour
+{
+    public static Kaiju Instance;
+
+    public bool IsInside = false;
+
+    SkeletonAnimation skeletonAnimation;
+    public string idleAnimation = "empty";
+    public string walkAnimation = "walk";
+    public string runAnimation = "walk";
+    public string hitAnimation = "hit";
+    public string deathAnimation = "death";
+    public float walkVelocity = 1;
+    public float runVelocity = 3;
+    public int hp = 10;
+    string currentAnimation = "";
+    bool hit = false;
+    bool dead = false;
+
+    void Start()
+    {
+        skeletonAnimation = GetComponent<SkeletonAnimation>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (!dead)
+        {
+            float x = Input.GetAxis("Horizontal");
+            float absX = Mathf.Abs(x);
+
+            //if (!hit)
+            //{
+
+            //}
+            //else
+            //{
+            //    if (skeletonAnimation.state.GetCurrent(0).Animation.Name != hitAnimation)
+            //        hit = false;
+            //}
+
+            float y = Input.GetAxis("Vertical");
+            float absY = Mathf.Abs(y);
+
+            Vector3 xy = new Vector3(Input.GetAxis("Horizontal"),0f,Input.GetAxis("Vertical")).normalized;
+
+            if (!hit)
+            {
+                if (x > 0)
+                    skeletonAnimation.skeleton.FlipX = false;
+                else if (x < 0)
+                    skeletonAnimation.skeleton.FlipX = true;
+
+                if (absX > 0.7f)
+                {
+                    SetAnimation(runAnimation, true);
+                    //GetComponent<Rigidbody>().velocity = new Vector3(runVelocity * Mathf.Sign(x), GetComponent<Rigidbody>().velocity.y, GetComponent<Rigidbody>().velocity.z);
+                }
+                else if (absX > 0)
+                {
+                    SetAnimation(walkAnimation, true);
+                   // GetComponent<Rigidbody>().velocity = new Vector3(walkVelocity * Mathf.Sign(x), GetComponent<Rigidbody>().velocity.y, GetComponent<Rigidbody>().velocity.z);
+                }
+
+
+                if (absY > 0.7f)
+                {
+                    SetAnimation(runAnimation, true);
+                   // GetComponent<Rigidbody>().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x, GetComponent<Rigidbody>().velocity.y, runVelocity * Mathf.Sign(y));
+                }
+                else if (absY > 0)
+                {
+                    SetAnimation(walkAnimation, true);
+                   // GetComponent<Rigidbody>().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x, GetComponent<Rigidbody>().velocity.y, walkVelocity * Mathf.Sign(y));
+                }
+
+                GetComponent<Rigidbody>().velocity = new Vector3(walkVelocity * xy.x, GetComponent<Rigidbody>().velocity.y, walkVelocity * xy.z);
+
+                if (absX <= 0 && absY <= 0)
+                {
+                    SetAnimation(idleAnimation, true);
+                    GetComponent<Rigidbody>().velocity = new Vector3(0, GetComponent<Rigidbody>().velocity.y, 0);
+                }
+            }
+            else
+            {
+                if (skeletonAnimation.state.GetCurrent(0).Animation.Name != hitAnimation)
+                    hit = false;
+            }
+
+
+        }
+    }
+
+    void SetAnimation(string anim, bool loop)
+    {
+        if (currentAnimation != anim)
+        {
+            skeletonAnimation.state.SetAnimation(0, anim, loop);
+            currentAnimation = anim;
+        }
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        //if (other.GetComponent<Room>() != null)
+        //{
+        //    IsInside = Room.IsInside;
+        //}
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        //if (other.GetComponent<Room>() != null)
+        //{
+        //}
+    }
+}
---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+
+public class ObjectPool : MonoBehaviour
+{
+    [Serializable]
+    public class PoolPrefab
+    {
+        public GameObject Prefab;
+        public int Amount;
+    }
+
+    public PoolPrefab[] Prefabs;  
+
+	// Use this for initialization
+	public virtual void Start () {
+	    foreach (PoolPrefab p in Prefabs)
+	    {
+	        GameObject instants = new GameObject(p.Prefab.name);
+            instants.transform.SetParent(transform);
+
+	        for (int i = 0; i < p.Amount; i++)
+	        {
+	            GameObject newInstant = (GameObject)Instantiate(p.Prefab, transform.position, Quaternion.identity);
+	            newInstant.name = p.Prefab.name + " - " + i;
+                InitInstance(newInstant);
+                newInstant.transform.SetParent(instants.transform);
+	        }
+	    }
+	}
+	
+	// Update is called once per frame
+	public virtual void Update ()
+	{
+	    
+
+	}
+
+    public virtual GameObject GetOne(string prefabName)
+    {
+        var instants = transform.FindChild(prefabName);
+        if (instants == null) return null;
+
+        for (int i = 0; i < instants.childCount; i++)
+        {
+            if (!instants.GetChild(i).gameObject.activeSelf) return instants.GetChild(i).gameObject;
+        }
+
+        return null;
+    }
+
+    internal virtual void InitInstance(GameObject instance)
+    {
+        
+    }
+}
