1a0d8f418d7b0c7552db11a84ade61fe0e8699d6
Carl Hughes <devilqube42@gmail.com>
2015-08-23 22:35:46 +0100

    Building Texture swap

--- Assets/Skyscraper.cs
+++ Assets/Skyscraper.cs
@@ -6,19 +6,78 @@ public class Skyscraper : MonoBehaviour
     public GameObject SectionPrefab;
 
     public int Height;
+    public Material BaseMaterial;
+    public Material MidMaterial;
+    public Material[] TopMaterial;
+    public Material BillBoardMaterial;
+    public GameObject BillBoardBoard;
+    public Material[] BillBoardLogoMaterial;
+   
+    public bool hasBillBoard = false;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
 	{
 	    Height = Random.Range(5, 10);
+        float tint = Random.Range(0.7f, 1.2f);
 
-	    float y = 0.25f;
+
+        Color tintColour = new Color(tint, tint, tint);
+      
+
+	    float y = 0.193f; // half the section height
 	    for (int i = 0; i < Height; i++)
 	    {
+            Debug.Log("Start() Building Building");
 	        var section = Instantiate(SectionPrefab);
             section.transform.SetParent(transform, false);
             section.transform.localPosition = new Vector3(0f,y,0f);
-	        y += 0.5f;
+            Renderer renderer = section.GetComponent<Renderer>();
+          
+            if (i == 0)
+            {
+                renderer.sharedMaterial= BaseMaterial;
+            }
+            
+            else if (i==Height-1)
+            {
+
+
+                int seed = Random.Range(0, TopMaterial.Length);
+
+                Material mat = TopMaterial[seed];
+
+                hasBillBoard=mat.name.Equals("ok-top2");
+
+                renderer.sharedMaterial =mat;
+
+                if (hasBillBoard)
+                {
+                    GameObject billBoard = Instantiate(BillBoardBoard);
+                    billBoard.transform.SetParent(section.transform, false);
+                    int billSeed = Random.Range(0, BillBoardLogoMaterial.Length);
+
+                  mat = BillBoardLogoMaterial[billSeed];
+                     renderer = billBoard.GetComponent<Renderer>();
+                    renderer.sharedMaterial = mat;
+
+                }
+
+            }
+            else
+            {
+                renderer.sharedMaterial = MidMaterial;
+            }
+
+
+
+
+            renderer.material.SetColor("_Color", tintColour);
+
+
+	        y += 0.386f; // Scale of section
+
+
 	        //if (i >= Height - 3)
 	        //{
          //       // Get rid of the climb trigger on the top section
